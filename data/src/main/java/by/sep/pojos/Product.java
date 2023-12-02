c45928965c5925a2ace99f0d46effa1f2a008ed0
abulgarin <a.bulgarin@gmail.com>
2023-12-02 18:17:32 +0300

    Added Many-to-Many relation between Client and Product Entities

---
+++ data/src/main/java/by/sep/pojos/Product.java
@@ -0,0 +1,77 @@
+package by.sep.pojos;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+@Entity
+@Table(name = "T_PRODUCT")
+public class Product {
+    @Id
+    @GenericGenerator(strategy = "uuid", name = "product_uuid")
+    @GeneratedValue(generator = "product_uuid")
+    @Column(name = "PRODUCT_ID")
+    private String id;
+    @Column(name = "PRODUCT_NAME")
+    private String name;
+    @ManyToMany(cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH})
+    @JoinTable(name = "T_CLIENT_PRODUCT",
+            joinColumns = @JoinColumn(name = "PRODUCT_ID"),
+            inverseJoinColumns = @JoinColumn(name = "CLIENT_ID"))
+    private List<Client> clients = new ArrayList<>();
+
+    public Product() {
+    }
+
+    public Product(String id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Client> getClients() {
+        return clients;
+    }
+
+    public void setClients(List<Client> clients) {
+        this.clients = clients;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Product product = (Product) o;
+
+        if (!Objects.equals(id, product.id)) return false;
+        if (!Objects.equals(name, product.name)) return false;
+        return Objects.equals(clients, product.clients);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        result = 31 * result + (clients != null ? clients.hashCode() : 0);
+        return result;
+    }
+}
